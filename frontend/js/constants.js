726068f0f1f875bb7796217e00a2db80dc818f46
Trypt Wang <detrypt@gmail.com>
2022-05-24 02:37:59 +0800

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0xa12564B499f8652Ff0D4d4d2477aD5E6ec950dF0"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
